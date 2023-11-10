1a94ec115e665e5d969c0b798c51aa6bd0420088
david <wizzardx@gmail.com>
2023-11-10 15:04:30 +0200

    Update the fetchNotes function to fetch an image if there is an image associated with a note

--- src/App.js
+++ src/App.js
@@ -28,6 +28,15 @@ const App = ({ signOut }) => {
   async function fetchNotes() {
     const apiData = await API.graphql({ query: listNotes });
     const notesFromAPI = apiData.data.listNotes.items;
+    await Promise.all(
+      notesFromAPI.map(async (note) => {
+        if (note.image) {
+          const url = await Storage.get(note.name);
+          note.image = url;
+        }
+        return note;
+      })
+    );
     setNotes(notesFromAPI);
   }
 
