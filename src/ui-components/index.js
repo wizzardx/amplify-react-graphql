fd0f72458a3b7e2b517ffa919d0103dd25e90232
david <wizzardx@gmail.com>
2023-11-10 14:57:13 +0200

    Ran 'amplify push -y --allow-destructive-graphql-schema-updates' to remove Todo from backend

--- src/ui-components/index.js
+++ src/ui-components/index.js
@@ -6,6 +6,4 @@
 
 export { default as NoteCreateForm } from "./NoteCreateForm";
 export { default as NoteUpdateForm } from "./NoteUpdateForm";
-export { default as TodoCreateForm } from "./TodoCreateForm";
-export { default as TodoUpdateForm } from "./TodoUpdateForm";
 export { default as studioTheme } from "./studioTheme";
