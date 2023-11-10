c6801d256c5b1add999b8a8f0456ea0e80a3c3e5
david <wizzardx@gmail.com>
2023-11-10 15:01:40 +0200

    Update UI code to import Storage and Image classes

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
 import React, { useState, useEffect } from "react";
 import "./App.css";
 import "@aws-amplify/ui-react/styles.css";
-import { API } from "aws-amplify";
+import { API, Storage } from 'aws-amplify';
 import {
   Button,
   Flex,
   Heading,
+  Image,
   Text,
   TextField,
   View,
   withAuthenticator,
-} from "@aws-amplify/ui-react";
+} from '@aws-amplify/ui-react';
 import { listNotes } from "./graphql/queries";
 import {
   createNote as createNoteMutation,
